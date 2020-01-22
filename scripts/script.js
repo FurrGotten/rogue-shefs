39e601193425ca85333aaffe6438fd4c70311cd6
FurrG <A2674151a>
2020-01-22 21:13:04 +0200

    add popups

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+document.getElementById("vine-1").addEventListener("click", popUp1);
+document.getElementById("vine-exit-1").addEventListener("click", popUp1);
+document.getElementById("vine-2").addEventListener("click", popUp2);
+document.getElementById("vine-exit-2").addEventListener("click", popUp2);
+document.getElementById("vine-3").addEventListener("click", popUp3);
+document.getElementById("vine-exit-3").addEventListener("click", popUp3);
+document.getElementById("vine-4").addEventListener("click", popUp4);
+document.getElementById("vine-exit-4").addEventListener("click", popUp4);
+document.getElementById("vine-5").addEventListener("click", popUp5);
+document.getElementById("vine-exit-5").addEventListener("click", popUp5);
+document.getElementById("vine-6").addEventListener("click", popUp6);
+document.getElementById("vine-exit-6").addEventListener("click", popUp6);
+document.getElementById("vine-7").addEventListener("click", popUp7);
+document.getElementById("vine-exit-7").addEventListener("click", popUp7);
+document.getElementById("vine-8").addEventListener("click", popUp8);
+document.getElementById("vine-exit-8").addEventListener("click", popUp8);
+
+
+function popUp1(){$("#vine-desk-1").toggleClass("vine-desk");}
+function popUp2(){$("#vine-desk-2").toggleClass("vine-desk");}
+function popUp3(){$("#vine-desk-3").toggleClass("vine-desk");}
+function popUp4(){$("#vine-desk-4").toggleClass("vine-desk");}
+function popUp5(){$("#vine-desk-5").toggleClass("vine-desk");}
+function popUp6(){$("#vine-desk-6").toggleClass("vine-desk");}
+function popUp7(){$("#vine-desk-7").toggleClass("vine-desk");}
+function popUp8(){$("#vine-desk-8").toggleClass("vine-desk");}
+
+
+
